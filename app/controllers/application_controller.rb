e9632da6cc4b1d856b950f5ee36124256dbabe77
Cassio Almeida <jcassiocosta@gmail.com>
2018-12-16 15:39:55 -0300

    fix locale to devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!
-
+  layout :layout_by_resource
+  
   def default_url_options
     { locale: I18n.locale }
   end
   
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header
   end
+
+  def layout_by_resource
+    devise_controller? ? 'devise' : 'application'
+  end
+
+  private
+    def extract_locale_from_accept_language_header
+      accept_language = request.env['HTTP_ACCEPT_LANGUAGE'] || ""
+      accept_language.match(/^[a-z]{2}(-[A-Z]{2})?/)[0]
+    end
 end
