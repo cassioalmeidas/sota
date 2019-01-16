23ee2298d1f41788f370a5c91b7b571de9baab8b
Cassio Almeida <jcassiocosta@gmail.com>
2019-01-16 19:17:12 -0300

    removed I18n::JS middleware from application config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Nitlabsota
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.middleware.use I18n::JS::Middleware
     config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
